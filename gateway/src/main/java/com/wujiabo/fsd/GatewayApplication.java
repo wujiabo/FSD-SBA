20e377f9d27c3f9273977cce413dfd62186c2e2e
Jiabo.Wu <wujbow@cn.ibm.com>
2019-10-31 19:31:58 +0800

    jwt

--- gateway/src/main/java/com/wujiabo/fsd/GatewayApplication.java
+++ gateway/src/main/java/com/wujiabo/fsd/GatewayApplication.java
@@ -4,9 +4,11 @@ package com.wujiabo.fsd;
 import com.wujiabo.fsd.filter.TokenFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
+@EnableFeignClients
 @EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication {
