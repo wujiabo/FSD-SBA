960bbd842e7f0c0a4cb7950e83e0705972528d41
Jiabo.Wu <wujbow@cn.ibm.com>
2019-11-03 09:39:50 +0800

    frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  gatewayurl: 'http://9.112.134.36:30090'
+  gatewayurl: 'http://127.0.0.1:9002'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  gatewayurl: 'http://localhost:9000'
+  gatewayurl: 'http://localhost:9002'
 };
 
 /*
